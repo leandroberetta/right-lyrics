311816f11f179b99f4e93b72e9b0c6ce34d2f747
Leandro Beretta <lberetta@redhat.com>
2019-09-13 12:24:49 -0300

    hits service

---
+++ hits-service/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import redis
+
+redis = redis.Redis(decode_responses=True)
+app = Flask(__name__)
+
+@app.route('/api/hits/<id>')
+def get(id):
+    return {"status": "0", "hits": str(redis.get(id)), "total": str(redis.get("total"))}
+
+@app.route('/api/hits', methods=['POST'])
+def hit():
+    data = request.json
+
+    redis.setnx(data['id'], 0)
+    redis.incr(data['id'], 1)
+
+    redis.setnx('total', 0)
+    redis.incr('total', 1)
+
+    return {"status": "0", "data": "ok"}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
