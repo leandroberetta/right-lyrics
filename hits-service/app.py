33891babdfd2df847c6b47d638004a6cab350979
Leandro Beretta <lberetta@redhat.com>
2019-09-13 15:07:09 -0300

    operator updated to create the hits service and hits redis

--- hits-service/app.py
+++ hits-service/app.py
@@ -2,33 +2,43 @@ from flask import Flask, request
 import redis
 import os
 
-redis = redis.Redis(decode_responses=True, password=os.environ["DB_PASSWORD"], host=os.environ["DB_HOST"])
+r = redis.Redis(decode_responses=True, password=os.environ["DB_PASSWORD"], host=os.environ["DB_HOST"])
 app = Flask(__name__)
 
 @app.route('/api/hits/<id>')
 def get(id):
-    return {"status": "0", "hits": str(redis.get(id)), "total": str(redis.get("total"))}
-
+    try :
+        return {"status": "0", "hits": str(r.get(id)), "total": str(r.get("total")), "message": "ok"}
+    except redis.ConnectionError as e:
+        return {"status": "-1", "message": str(e)}
+    
 @app.route('/api/popularity/<id>')
 def get_popularity(id):
-    hits = int(redis.get(id))
-    total = int(redis.get('total'))
+    try: 
+        hits = int(r.get(id))
+        total = int(r.get('total'))
 
-    ratio = hits * 5 / total
+        ratio = hits * 5 / total
 
-    return {"status": "0", "popularity": str(round(ratio, 2)) + '/5'}
+        return {"status": "0", "popularity": str(round(ratio, 2)) + '/5', "message": "ok"}
+    except redis.ConnectionError as e:
+        return {"status": "-1", "message": str(e)}    
 
 @app.route('/api/hits', methods=['POST'])
 def hit():
-    data = request.json
+    try:
+        data = request.json
 
-    redis.setnx(data['id'], 0)
-    redis.incr(data['id'], 1)
+        r.setnx(data['id'], 0)
+        r.incr(data['id'], 1)
 
-    redis.setnx('total', 0)
-    redis.incr('total', 1)
+        r.setnx('total', 0)
+        r.incr('total', 1)
 
-    return {"status": "0", "data": "ok"}
+        return {"status": "0", "message": "ok"}
+    except redis.ConnectionError as e:
+        return {"status": "-1", "message": str(e)}
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
