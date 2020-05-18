d658dbe82e2a3e9501690780b9e17ce25ccb4db7
Adriel Paredes <adriel.paredes@gmail.com>
2020-05-18 10:26:04 -0300

    RL-8: Se agrega pantalla para No Data

--- lyrics-admin-ui/src/pages/albums/Albums.tsx
+++ lyrics-admin-ui/src/pages/albums/Albums.tsx
@@ -1,42 +1,12 @@
 import { inject, observer } from "mobx-react";
 import React from "react";
+import request from "superagent";
 import AlbumsService from "../../api/AlbumsService";
+import NoData from "../../common/NoData";
 import Search from "../../common/Search";
+import SearchResponse from "../../model/SearchResponse";
 import { HeaderStore } from "../../store/HeaderStore";
 import Album from "./Album";
-import request, { SuperAgent } from "superagent";
-import SearchResponse from "../../model/SearchResponse";
-
-const albums = [
-  {
-    title: "Congrete and Gold",
-    artist: "Foo Fighters",
-    cover:
-      "https://www.elquintobeatle.com/wp-content/uploads/2017/09/foo-fighters-concrete-and-gold-1.jpg",
-    year: "2018",
-  },
-  {
-    title: "Californication",
-    artist: "RHCP",
-    cover:
-      "https://http2.mlstatic.com/red-hot-chili-peppers-californication-vinilo-nuevo-obivinilo-D_NQ_NP_150511-MLC20572622222_022016-F.jpg",
-    year: "1999",
-  },
-  {
-    title: "Are You Experienced?",
-    artist: "Jimmy Hendrix Experience",
-    cover:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Are_You_Experienced_-_US_cover-edit.jpg/800px-Are_You_Experienced_-_US_cover-edit.jpg",
-    year: "1967",
-  },
-  {
-    title: "1 (Remastered)",
-    artist: "The Beatles",
-    cover:
-      "https://images-na.ssl-images-amazon.com/images/I/91-Uov6izOL._SL1500_.jpg",
-    year: "2000",
-  },
-];
 
 interface AlbumsProps {
   headerStore?: HeaderStore;
@@ -57,22 +27,31 @@ class Albums extends React.Component<AlbumsProps> {
     service.getAll(0, 0, "").then(
       (res: request.Response) => {
         const response: SearchResponse<Album> = res.body;
-        console.log(response);
         this.setState({ albums: response.data });
       },
       (err) => {}
     );
   }
 
-  render() {
-    return (
-      <div className="container-fluid albums">
-        <Search></Search>
-        <div className="albums__section">
+  buildAlbums = () => {
+    if (this.state.albums.length > 0) {
+      return (
+        <div className="albums__board">
           {this.state.albums.map((elem) => (
             <Album album={elem}></Album>
           ))}
         </div>
+      );
+    } else {
+      return <NoData content="No se encontraron albums"></NoData>;
+    }
+  };
+
+  render() {
+    return (
+      <div className="container-fluid albums">
+        <Search></Search>
+        <div className="albums__section">{this.buildAlbums()}</div>
       </div>
     );
   }
