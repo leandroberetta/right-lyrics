07275f6709ddfd69e8364b9787c8b7dca15ddbed
Leandro Beretta <lea.beretta@gmail.com>
2020-07-20 01:49:55 -0300

    fixes

--- documentation/openshift/install.sh
+++ documentation/openshift/install.sh
@@ -1,7 +1,5 @@
 #!/user/bin/env bash
 
-# INT
-
 oc create namespace right-lyrics
 
 echo "apiVersion: v1
@@ -24,9 +22,12 @@ oc secret link default redhat-credentials --for=pull -n right-lyrics
 oc apply -f https://raw.githubusercontent.com/leandroberetta/karpenter/master/tasks/git/git.yaml -n right-lyrics
 oc apply -f https://raw.githubusercontent.com/leandroberetta/karpenter/master/tasks/s2i/s2i.yaml -n right-lyrics
 oc apply -f https://raw.githubusercontent.com/leandroberetta/karpenter/master/tasks/npm/npm.yaml -n right-lyrics
+oc apply -f https://raw.githubusercontent.com/leandroberetta/karpenter/master/tasks/mvn/mvn.yaml -n right-lyrics
 oc apply -f https://raw.githubusercontent.com/leandroberetta/karpenter/master/tasks/kubectl/kubectl.yaml -n right-lyrics
 
 oc apply -f albums-service/k8s/albums-pipeline.yaml -n right-lyrics
+oc apply -f hits-service/k8s/hits-pipeline.yaml -n right-lyrics
+oc apply -f lyrics-page/k8s/page-pipeline.yaml -n right-lyrics
 
 tkn pipeline start albums-pipeline -n right-lyrics \
   -w name=source,claimName=source,subPath=albums \
@@ -34,10 +35,14 @@ tkn pipeline start albums-pipeline -n right-lyrics \
   -p GIT_REVISION=master \
   -p IMAGE=image-registry.openshift-image-registry.svc:5000/right-lyrics/albums-service:latest
 
-oc apply -f hits-service/k8s/hits-pipeline.yaml -n right-lyrics
-
 tkn pipeline start hits-pipeline -n right-lyrics \
   -w name=source,claimName=source,subPath=hits \
   -p GIT_REPOSITORY=https://github.com/leandroberetta/right-lyrics \
   -p GIT_REVISION=master \
   -p IMAGE=image-registry.openshift-image-registry.svc:5000/right-lyrics/hits-service:latest
+
+tkn pipeline start page-pipeline -n right-lyrics \
+  -w name=source,claimName=source,subPath=page \
+  -p GIT_REPOSITORY=https://github.com/leandroberetta/right-lyrics \
+  -p GIT_REVISION=master \
+  -p IMAGE=image-registry.openshift-image-registry.svc:5000/right-lyrics/lyrics-page:latest
