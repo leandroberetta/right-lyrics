504855e51647a88b6388856852d2e78a4997aae9
Leandro Beretta <lea.beretta@gmail.com>
2020-07-21 14:17:43 -0300

    lyrics kustomize

--- documentation/minikube/install.sh
+++ documentation/minikube/install.sh
@@ -69,7 +69,17 @@ tkn pipeline start hits-pipeline \
   -n right-lyrics \
   --showlog 
 
-kubectl apply -f hits-service/k8s/overlays/dev/page-pipeline.yaml -n right-lyrics
+kubectl apply -f lyrics-service/k8s/overlays/dev/lyrics-pipeline.yaml -n right-lyrics
+
+tkn pipeline start lyrics-pipeline \
+  -s pipeline \
+  -w name=source,claimName=source,subPath=lyrics \
+  -p GIT_REPOSITORY=https://github.com/leandroberetta/right-lyrics \
+  -p GIT_REVISION=master \
+  -n right-lyrics \
+  --showlog 
+  
+kubectl apply -f lyrics-page/k8s/overlays/dev/page-pipeline.yaml -n right-lyrics
 
 tkn pipeline start page-pipeline \
   -s pipeline \
