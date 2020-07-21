39dab51a8b20d9409da7f7a89551e3df0d8f27e8
Leandro Beretta <lea.beretta@gmail.com>
2020-07-21 14:21:56 -0300

    songs kustomize

--- documentation/minikube/install.sh
+++ documentation/minikube/install.sh
@@ -56,8 +56,7 @@ tkn pipeline start albums-pipeline \
   -w name=source,claimName=source,subPath=albums \
   -p GIT_REPOSITORY=https://github.com/leandroberetta/right-lyrics \
   -p GIT_REVISION=master \
-  -n right-lyrics \
-  --showlog 
+  -n right-lyrics
 
 kubectl apply -f hits-service/k8s/overlays/dev/hits-pipeline.yaml -n right-lyrics
 
@@ -66,8 +65,7 @@ tkn pipeline start hits-pipeline \
   -w name=source,claimName=source,subPath=hits \
   -p GIT_REPOSITORY=https://github.com/leandroberetta/right-lyrics \
   -p GIT_REVISION=master \
-  -n right-lyrics \
-  --showlog 
+  -n right-lyrics
 
 kubectl apply -f lyrics-service/k8s/overlays/dev/lyrics-pipeline.yaml -n right-lyrics
 
@@ -76,9 +74,8 @@ tkn pipeline start lyrics-pipeline \
   -w name=source,claimName=source,subPath=lyrics \
   -p GIT_REPOSITORY=https://github.com/leandroberetta/right-lyrics \
   -p GIT_REVISION=master \
-  -n right-lyrics \
-  --showlog 
-  
+  -n right-lyrics
+
 kubectl apply -f lyrics-page/k8s/overlays/dev/page-pipeline.yaml -n right-lyrics
 
 tkn pipeline start page-pipeline \
@@ -86,5 +83,4 @@ tkn pipeline start page-pipeline \
   -w name=source,claimName=source,subPath=page \
   -p GIT_REPOSITORY=https://github.com/leandroberetta/right-lyrics \
   -p GIT_REVISION=master \
-  -n right-lyrics \
-  --showlog 
+  -n right-lyrics
