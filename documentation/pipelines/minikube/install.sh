d12383f248a90cb5467faa70d5bf1424c84a3102
Leandro Beretta <lea.beretta@gmail.com>
2020-07-25 18:58:30 -0300

    importer renamed to importe, kustomize

--- documentation/pipelines/minikube/install.sh
+++ documentation/pipelines/minikube/install.sh
@@ -73,6 +73,7 @@ kubectl apply -f albums-service/k8s/overlays/dev/albums-pipeline.yaml -n right-l
 kubectl apply -f hits-service/k8s/overlays/dev/hits-pipeline.yaml -n right-lyrics
 kubectl apply -f lyrics-service/k8s/overlays/dev/lyrics-pipeline.yaml -n right-lyrics
 kubectl apply -f songs-service/k8s/overlays/dev/songs-pipeline.yaml -n right-lyrics
+kubectl apply -f import-service/k8s/overlays/dev/import-pipeline.yaml -n right-lyrics
 kubectl apply -f lyrics-ui/k8s/overlays/dev/ui-pipeline.yaml -n right-lyrics
 
 tkn pipeline start albums-pipeline \
@@ -99,6 +100,13 @@ tkn pipeline start lyrics-pipeline \
 tkn pipeline start songs-pipeline \
   -s pipeline \
   -w name=source,claimName=source,subPath=songs \
+  -p GIT_REPOSITORY=https://github.com/leandroberetta/right-lyrics \
+  -p GIT_REVISION=master \
+  -n right-lyrics
+
+tkn pipeline start import-pipeline \
+  -s pipeline \
+  -w name=source,claimName=source,subPath=import \
   -p GIT_REPOSITORY=https://github.com/leandroberetta/right-lyrics \
   -p GIT_REVISION=master \
   -n right-lyrics
