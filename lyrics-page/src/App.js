745a40a3cc99b4acff8ba0d332f47a20aedc2c31
Leandro Beretta <lea.beretta@gmail.com>
2020-08-23 21:01:56 -0300

    fix jsx identation

--- lyrics-page/src/App.js
+++ lyrics-page/src/App.js
@@ -108,49 +108,43 @@ class App extends React.Component {
         var mainSection = null;
 
         if (this.state.selectedSong) {
-            mainSection = ( <
-                div >
-                <
-                SongItem onDeselectSong = { this.onDeselectSong }
-                onSelectSong = { this.onSelectSong }
-                authenticated = { this.state.authenticated }
-                key = { this.state.selectedSong.id }
-                song = { this.state.selectedSong }
-                /> <
-                SongLyrics lyrics = { this.state.selectedSong.lyrics }
-                /> < /
-                div >
+            mainSection = (
+                <div>
+                    <SongItem onDeselectSong={this.onDeselectSong}
+                        onSelectSong={this.onSelectSong}
+                        authenticated={this.state.authenticated}
+                        key={this.state.selectedSong.id}
+                        song={this.state.selectedSong} />
+                    <SongLyrics lyrics={this.state.selectedSong.lyrics} />
+                </div>
             );
         } else {
-            mainSection = ( <
-                SongList authenticated = { this.state.authenticated }
-                onSelectSong = { this.onSelectSong }
-                songs = { this.state.songs }
-                />
+            mainSection = (
+                <SongList authenticated={this.state.authenticated}
+                    onSelectSong={this.onSelectSong}
+                    songs={this.state.songs} />
             );
         }
 
         if (this.state.error) {
-            errorSection = ( <
-                Alert variant = "danger" >
-                <
-                Alert.Heading > Error! < /Alert.Heading> <
-                p > { this.state.error } < /p> < /
-                Alert >
+            errorSection = (
+                <Alert variant="danger">
+                    <Alert.Heading > Error! </Alert.Heading>
+                    <p>{this.state.error}</p>
+                </Alert>
             );
         }
 
-        return ( <
-            Container className = "padding" >
-            <
-            NavBar authenticated = { this.state.authenticated }
-            keycloak = { this.state.keycloak }
-            onSearch = { this.onSearch }
-            /> { mainSection } { errorSection } < /
-            Container >
+        return (
+            <Container className="padding">
+                <NavBar authenticated={this.state.authenticated}
+                    keycloak={this.state.keycloak}
+                    onSearch={this.onSearch} />
+                {mainSection}
+                {errorSection}
+            </Container>
         );
     };
-
 }
 
 export default App;
